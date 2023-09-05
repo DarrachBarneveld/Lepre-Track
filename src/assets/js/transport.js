8c0a05332285b2047c210450eab713002f6ff436
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-05 23:22:29 +0100

    Added bar chart

--- src/assets/js/transport.js
+++ src/assets/js/transport.js
@@ -6,7 +6,7 @@ const transportForm = document.getElementById("transportForm");
 const kilometers = document.getElementById("kilometers");
 
 const DUMMY_DATA = {
-  // this is the average carbon per week of a car
+  // this is the average milage/carbon per week of a car
   averageKM: 327,
 };
 
@@ -41,10 +41,35 @@ async function logData(e) {
   );
 
   let color;
-  percentOfCarKM > 100 ? (color = "#FF0000") : (color = "#569ef9");
+  //   percentOfCarKM > 100 ? (color = "#FF0000") : (color = "#569ef9");
+
+  percentOfCarKM > 100 ? (color = "#FF0000") : (color = "#00E396");
+
+  //   chart.updateOptions({
+  //     series: [percentOfCarKM / 2, options.series[1]],
+  //     colors: [color, options.colors[1]],
+  //   });
 
   chart.updateOptions({
-    series: [percentOfCarKM / 2, options.series[1]],
+    series: [
+      {
+        name: "You",
+        data: [
+          {
+            x: "Car",
+            y: totalKilometers.toFixed(2),
+            goals: [
+              {
+                name: "Average",
+                value: 367,
+                strokeHeight: 5,
+                strokeColor: "#775DD0",
+              },
+            ],
+          },
+        ],
+      },
+    ],
     colors: [color, options.colors[1]],
   });
 
@@ -57,37 +82,87 @@ async function logData(e) {
 
 transportForm.addEventListener("submit", logData);
 
+// RADIAL CHART
+
+// const radialOptions = {
+//   series: [10, 50],
+//   chart: {
+//     height: 350,
+//     type: "radialBar",
+//   },
+//   plotOptions: {
+//     radialBar: {
+//       dataLabels: {
+//         name: {
+//           fontSize: "22px",
+//         },
+//         value: {
+//           fontSize: "16px",
+//           formatter: function (val, t) {
+//             console.log(t);
+//             const intVal = +val * 2;
+//             return intVal.toFixed(2) + "%";
+//           },
+//         },
+//         total: {
+//           show: true,
+//           label: "Average Km",
+//           formatter: function (item) {
+//             return 367; // Average amount of km per week Ireland
+//           },
+//         },
+//       },
+//     },
+//   },
+//   labels: ["You", "Average"],
+//   colors: ["#569ef9", "#58e5a4"],
+// };
+
+// const radialChart = new ApexCharts(
+//   document.querySelector("#chart"),
+//   radialOptions
+// );
+// radialChart.render();
+
+// BAR CHART
+
 const options = {
-  series: [10, 50],
+  series: [
+    {
+      name: "You",
+      data: [
+        {
+          x: "Car",
+          y: 10,
+          goals: [
+            {
+              name: "Average",
+              value: 367,
+              strokeHeight: 5,
+              strokeColor: "#775DD0",
+            },
+          ],
+        },
+      ],
+    },
+  ],
   chart: {
     height: 350,
-    type: "radialBar",
+    type: "bar",
   },
-  plotOptions: {
-    radialBar: {
-      dataLabels: {
-        name: {
-          fontSize: "22px",
-        },
-        value: {
-          fontSize: "16px",
-          formatter: function (val) {
-            const intVal = +val * 2;
-            return intVal.toFixed(2) + "%";
-          },
-        },
-        total: {
-          show: true,
-          label: "Average Km",
-          formatter: function () {
-            return 367; // Average amount of km per week Ireland
-          },
-        },
-      },
+
+  colors: ["#00E396"],
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: true,
+    showForSingleSeries: true,
+    customLegendItems: ["You", "Average"],
+    markers: {
+      fillColors: ["#00E396", "#775DD0"],
     },
   },
-  labels: ["You", "Average"],
-  colors: ["#569ef9", "#58e5a4"],
 };
 
 const chart = new ApexCharts(document.querySelector("#chart"), options);
