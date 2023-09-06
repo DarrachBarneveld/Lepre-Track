733b82ab77109c54003bb0cbb5e9eb96eead2cf1
Shane-Donlon <shane.donlon@live.com>
2023-09-06 12:32:01 +0100

    Script add: call variables

---
+++ src/assets/js/energy.js
@@ -0,0 +1,11 @@
+let electricity = 0;
+let gas = 0;
+let oil = 0;
+let coal = 0;
+let lpg = 0;
+let propane = 0;
+let wood = 0;
+
+let input_form = document.querySelector(".energy_form");
+let btn = document.querySelector(".btn");
+let inputs = document.querySelectorAll("input");
