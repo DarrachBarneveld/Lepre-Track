2958d6f35b46ad4139d1ab41ead790b40c7cc93b
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-07 05:02:24 +0100

    Created classes for dashboard radial, added color func

--- src/assets/classes/Charts.js
+++ src/assets/classes/Charts.js
@@ -1,72 +1,101 @@
-export const defaultBarChartOptions = {
-  series: [
-    {
-      name: "You",
-      data: [
-        {
-          x: "Car",
-          y: 10,
-          goals: [
-            {
-              name: "Average",
-              value: 367,
-              strokeHeight: 5,
-              strokeColor: "#775DD0",
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  chart: {
-    height: 350,
-    type: "bar",
-  },
+function graphColor(num) {
+  if (num < 25) {
+    return ["#FF0000"];
+  } else if (num < 35) {
+    return ["#ff8c00"];
+  } else if (num < 70) {
+    return ["#FFBF00"];
+  }
+  return ["#008000"];
+}
 
-  colors: ["#00E396"],
-  dataLabels: {
-    enabled: false,
-  },
-  legend: {
-    show: true,
-    showForSingleSeries: true,
-    customLegendItems: ["You", "Average"],
-    markers: {
-      fillColors: ["#00E396", "#775DD0"],
-    },
-  },
-};
-
-export const defaultRadialOptions = {
-  series: [10, 50],
-  chart: {
-    height: 350,
-    type: "radialBar",
-  },
-  plotOptions: {
-    radialBar: {
-      dataLabels: {
-        name: {
-          fontSize: "22px",
-        },
-        value: {
-          fontSize: "16px",
-          formatter: function (val, t) {
-            console.log(t);
-            const intVal = +val * 2;
-            return intVal.toFixed(2) + "%";
+export class DashboardRadialBarChartOptions {
+  constructor(series) {
+    this.series = series;
+    this.colors = graphColor(series[0]);
+    this.chart = {
+      height: 200,
+      type: "radialBar",
+      offsetY: -10,
+    };
+    this.plotOptions = {
+      radialBar: {
+        track: {
+          show: true,
+          background: "#fff",
+          strokeWidth: "100%",
+          dropShadow: {
+            enabled: true,
+            top: 0,
+            left: 0,
+            blur: 1,
+            opacity: 1,
           },
         },
-        total: {
-          show: true,
-          label: "Average Km",
-          formatter: function (item) {
-            return 367; // Average amount of km per week Ireland
+        startAngle: -140,
+        endAngle: 140,
+        dataLabels: {
+          name: {
+            show: false,
+          },
+          value: {
+            offsetY: 10,
+            fontSize: "22px",
+            color: undefined,
+            formatter: function (val) {
+              return val + "%";
+            },
           },
         },
       },
-    },
-  },
-  labels: ["You", "Average"],
-  colors: ["#569ef9", "#58e5a4"],
-};
+    };
+    this.stroke = {
+      dashArray: 0,
+    };
+  }
+}
+
+// var options = {
+//   series: [100],
+//   colors: ["#008000"],
+//   chart: {
+//     height: 200,
+//     type: "radialBar",
+//     offsetY: -10,
+//   },
+//   plotOptions: {
+//     radialBar: {
+//       track: {
+//         show: true,
+//         background: "#fff",
+//         strokeWidth: "100%",
+//         dropShadow: {
+//           enabled: true,
+//           top: 0,
+//           left: 0,
+//           blur: 1,
+//           opacity: 1,
+//         },
+//       },
+//       startAngle: -140,
+//       endAngle: 140,
+//       dataLabels: {
+//         name: {
+//           show: false,
+//         },
+//         value: {
+//           offsetY: 10,
+//           fontSize: "22px",
+//           color: undefined,
+//           formatter: function (val) {
+//             return val + "%";
+//           },
+//         },
+//       },
+//     },
+//   },
+
+//   stroke: {
+//     dashArray: 1,
+//   },
+// };
--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,7 +1,26 @@
-// import Swal from "sweetalert2";
-// import { getUserData, checkAuthState } from "./auth";
+import ApexCharts from "apexcharts";
+import { DashboardRadialBarChartOptions } from "../classes/Charts";
 
-// document.addEventListener("DOMContentLoaded", async function () {
-//   const user = await checkAuthState();
-//   const userData = await getUserData(user);
-// });
+const transportChart = new ApexCharts(
+  document.querySelector("#transportChart"),
+  new DashboardRadialBarChartOptions([67])
+);
+transportChart.render();
+
+const foodChart = new ApexCharts(
+  document.querySelector("#foodChart"),
+  new DashboardRadialBarChartOptions([20])
+);
+foodChart.render();
+
+const energyChart = new ApexCharts(
+  document.querySelector("#energyChart"),
+  new DashboardRadialBarChartOptions([100])
+);
+energyChart.render();
+
+const communityChart = new ApexCharts(
+  document.querySelector("#communityChart"),
+  new DashboardRadialBarChartOptions([33])
+);
+communityChart.render();
