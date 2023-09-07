2d6be46848898490f06a0259ed3e1083186db9f8
Shane-Donlon <shane.donlon@live.com>
2023-09-07 16:22:59 +0100

    Animation for tracking button add

---
+++ src/assets/js/index.js
@@ -0,0 +1,9 @@
+anime({
+  targets: ".start-tracking",
+  translateX: [5000, 0], // from 100 to 250
+  //   delay: 150,
+  duration: 1500,
+  direction: "normal",
+  borderRadius: 1000000,
+  easing: "easeOutBack",
+});
