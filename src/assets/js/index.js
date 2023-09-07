452cb7c9da415b368e55eef8ed30148e9d290f9a
Shane-Donlon <shane.donlon@live.com>
2023-09-07 16:47:24 +0100

    Animejs add

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,8 @@
 anime({
   targets: ".start-tracking",
-  translateX: [5000, 0], // from 100 to 250
+  translateY: [-1500, 0], // from 100 to 250
   //   delay: 150,
   duration: 1500,
   direction: "normal",
-  borderRadius: 1000000,
   easing: "easeOutBack",
 });
